598dfa3b6d71d841ee0d4242d6fa26995d3d6e1b
wasdns <952693358@qq.com>
2018-01-12 03:12:08 -0800

    Update listener: listen to all possible ports

--- demo/utils/listener.py
+++ demo/utils/listener.py
@@ -10,7 +10,7 @@ from scapy.all import IP, TCP, ICMP, UDP, Raw
 
 parser = argparse.ArgumentParser(description='Listener')
 parser.add_argument('-p', '--port', help='listen port', 
-                    type=str, action="store", default='s1-eth1')
+                    type=str, action="store", default='non')
 parser.add_argument('-s', '--switch-name', help='switch name',
                     type=str, action="store", default='s1')
 args = parser.parse_args()
@@ -36,9 +36,13 @@ def main():
     thrift_port = _THRIFT_BASE_PORT+index
 
     iface = args.port
+    if iface == "non":
+        iface = ["s3-eth3", "s3-eth4", "s4-eth3", "s4-eth4"]
+
     print "sniffing on %s" % iface
     sys.stdout.flush()
-    sniff(filter="udp", iface = iface, prn = lambda x: handle_udp_pkt(x, thrift_port))
+    # sniff(filter="udp", iface = iface, prn = lambda x: handle_udp_pkt(x, thrift_port))
+    sniff(filter="udp", iface=iface, prn=lambda x: x.sniffed_on+": "+x.summary())
 
 if __name__ == '__main__':
     main()
