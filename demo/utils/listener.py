7083fbfa3dbcbf6c266ec09a2a109480b825ae2b
wasdns <952693358@qq.com>
2018-01-13 00:53:17 -0800

    Update listener, listening to all ports

--- demo/utils/listener.py
+++ demo/utils/listener.py
@@ -19,6 +19,9 @@ _THRIFT_BASE_PORT = 22222
 
 counter = 0
 
+# listen port
+server1, server2, server3, server4 = 0,0,0,0
+
 def handle_tcp_pkt(packet, thrift_port):
     global counter
     counter = counter+1
@@ -28,6 +31,26 @@ def handle_udp_pkt(packet, thrift_port):
     global counter
     counter = counter+1
     return 'UDP Packet #{}: {} ==> {}, sport {} ==> dport {}'.format(counter, packet[0][1].src, packet[0][1].dst, packet[UDP].sport, packet[UDP].dport)
+
+def report(sniff_on):
+    global server1, server2, server3, server4
+    if sniff_on == "s3-eth3":
+        server1 += 1
+    elif sniff_on == "s3-eth4":
+        server2 += 1
+    elif sniff_on == "s4-eth3":
+        server3 += 1
+    elif sniff_on == "s4-eth4":
+        server4 += 1
+    else:
+        print "You are not listen to the default port lists."
+        print "Get a packet at %s" % sniff_on
+        return
+
+    print "\n============================================================"
+    print "s3-eth3: %d, s3-eth4: %d, s4-eth3: %d, s4-eth4: %d" % (server1, server2, server3, server4)
+    print "============================================================\n"
+        
 
 def main():
     # Get Thrift Port
@@ -42,7 +65,7 @@ def main():
     print "sniffing on %s" % iface
     sys.stdout.flush()
     # sniff(filter="udp", iface = iface, prn = lambda x: handle_udp_pkt(x, thrift_port))
-    sniff(filter="udp", iface=iface, prn=lambda x: x.sniffed_on+": "+x.summary())
+    sniff(filter="udp and port 340", iface=iface, prn=lambda x: report(x.sniffed_on))
 
 if __name__ == '__main__':
     main()
